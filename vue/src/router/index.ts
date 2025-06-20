3da8eb9630dd4085b849540997613f8ac10ae1a0
TwanAsselbergs <twanasselbergs@outlook.com>
2025-06-20 10:27:05 +0200

    Create Vue router

--- vue/src/router/index.ts
+++ vue/src/router/index.ts
@@ -1,7 +1,19 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "@/pages/Home.vue";
+import Experience from "@/pages/Experience.vue";
+import Agenda from "@/pages/Agenda.vue";
+import Disclaimer from "@/pages/Disclaimer.vue";
+import CookiePolicy from "@/pages/CookiePolicy.vue";
+import PrivacyPolicy from "@/pages/PrivacyPolicy.vue";
 
-const routes = [{ path: "/", component: Home }];
+const routes = [
+  { path: "/", component: Home },
+  { path: "/ervaring", component: Experience },
+  { path: "/agenda", component: Agenda },
+  { path: "/disclaimer", component: Disclaimer },
+  { path: "/cookiebeleid", component: CookiePolicy },
+  { path: "/privacy-policy", component: PrivacyPolicy },
+];
 
 export const router = createRouter({
   history: createWebHistory(),
